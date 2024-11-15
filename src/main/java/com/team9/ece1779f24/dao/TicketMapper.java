270e0fc158a64ce255e7787e464169fd5710a44f
Yilin Huai <yl815059@dal.ca>
2024-11-15 17:19:49 -0500

    TicketMapper add createTicket

--- src/main/java/com/team9/ece1779f24/dao/TicketMapper.java
+++ src/main/java/com/team9/ece1779f24/dao/TicketMapper.java
@@ -1,11 +1,14 @@
 package com.team9.ece1779f24.dao;
 
 import com.team9.ece1779f24.model.Ticket;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
 import org.springframework.stereotype.Repository;
 
 @Mapper
 @Repository
-public class TicketMapper {
+public interface TicketMapper {
     @Insert("INSERT INTO ticket (flight_id, passenger_id, ticket_number, " +
             "ticket_class, status, seat_number, price, booking_time, booking_reference) " +
             "VALUES (#{flightId}, #{passengerId}, #{ticketNumber}, " +
