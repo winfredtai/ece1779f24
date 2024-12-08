39f1c07162bc98df052aa571d590b41db96f6c82
Winfred <17zt12@queensu.ca>
2024-12-08 17:59:06 -0500

    "add ticket related controllers/service/DTOs; update gitignore file"

--- src/main/java/com/team9/ece1779f24/config/AppConstants.java
+++ src/main/java/com/team9/ece1779f24/config/AppConstants.java
@@ -4,5 +4,6 @@ public class AppConstants {
     public static final String PAGE_NUMBER = "0";
     public static final String PAGE_SIZE = "10";
     public static final String SORT_FLIGHT_BY = "flightNumber";
+    public static final String SORT_TICKET_BY = "ticketClass";
     public static final String SORT_DIR = "asc";
 }
--- src/main/java/com/team9/ece1779f24/payload/OrderRequestDTO.java
+++ src/main/java/com/team9/ece1779f24/payload/OrderRequestDTO.java
@@ -3,13 +3,15 @@ package com.team9.ece1779f24.payload;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import java.util.List;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class OrderRequestDTO {
     private Long userId;
-
+    private Long flightId;
+    private List<String> tickets;
     private String paymentMethod;
     private String paymentGatewayId;
     private String paymentGatewayStatus;
